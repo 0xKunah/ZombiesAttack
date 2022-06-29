ac2def338337123b29df2d7e82d18537b093bc60
Kunah <reply.kunah@gmail.com>
2022-06-29 23:53:57 +0200

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+
+namespace ZombiesAttack
+{
+    internal class Game
+    {
+        private static int round = 0;
+        private static int enemies = 5;
+        private List<Ped> attackers = new List<Ped>();
+
+        public Game()
+        {
+            while (GTA.Game.Player.IsAlive)
+            {
+                OnTick();
+                Script.Wait(1);
+            }
+        }
+
+        public void OnTick()
+        {
+            if (attackers.Count == 0)
+            {
+                round += 1;
+                enemies = (int)Math.Ceiling(enemies * 1.2);
+                SpawnEnemies(enemies, (int)(enemies/5));
+            };
+            for (int i = attackers.Count - 1; i >= 0; i--)
+            {
+                if (attackers[i].IsDead) attackers.RemoveAt(i);
+            }
+            GTA.UI.Screen.ShowSubtitle($"~r~Enemies left:{attackers.Count} ~s~|~b~ Round: {round}", 1);
+        }
+
+        public void SpawnEnemies(int total, int step)
+        {
+            for (int i = 0; i < total; i += step)
+            {
+                for (int j = 0; j < step; j++)
+                {
+                    Ped Attacker = World.CreatePed(PedHash.Zombie01, GTA.Game.Player.Character.Position.Around(25));
+                    attackers.Add(Attacker);
+                    Attacker.Task.FightAgainst(GTA.Game.Player.Character);
+                }
+                GTA.UI.Screen.ShowSubtitle($"~b~Enemies Spawning... ~r~Enemies left:{attackers.Count} ~s~|~b~ Round: {round}", 2000);
+                Script.Wait(2000);
+            };
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+using GTA;
+using GTA.Native;
+using NativeUI;
+
+namespace ZombiesAttack
+{
+    internal class Main : Script
+    {
+        internal static MenuPool _menuPool;
+        internal UIMenu mainMenu = new UIMenu("Zombies attack", "Select an option");
+        internal UIMenuItem putOnVRButton = new UIMenuItem("Put on VR headset");
+        internal UIMenuItem startGameButton = new UIMenuItem("Start a game");
+        public Main()
+        {
+            
+            mainMenu.AddItem(putOnVRButton);
+            mainMenu.AddItem(startGameButton);
+
+            mainMenu.OnItemSelect += ButtonHandler;
+
+            _menuPool = new MenuPool();
+            _menuPool.Add(mainMenu);
+
+            Tick += (o, e) =>
+            {
+                _menuPool.ProcessMenus();
+            };
+
+            KeyUp += (s, e) =>
+            {
+                if (e.KeyCode == Keys.NumPad6) mainMenu.Visible = !mainMenu.Visible;
+            };
+        }
+
+        private void ButtonHandler(dynamic sender, UIMenuItem item, int index)
+        {
+            if (item == startGameButton) StartGame();
+            else if (item == putOnVRButton) PutOnVRHeadset();
+        }
+
+        private void StartGame()
+        {
+            if (Function.Call<int>(Hash.GET_PED_PROP_INDEX, Function.Call<Ped>(Hash.GET_PLAYER_PED, -1), 0) == 16)
+            {
+                GTA.Game.Player.Character.Weapons.RemoveAll();
+                GTA.Game.Player.Character.Weapons.Give(WeaponHash.SNSPistol, 24, true, true);
+                Game game = new Game();
+            }
+            else GTA.UI.Notification.Show("Put on a ~b~VR Headset~s~ before starting a game");
+        }
+
+        private void PutOnVRHeadset()
+        {
+            Function.Call<Ped>(Hash.SET_PED_PROP_INDEX, Function.Call<Ped>(Hash.GET_PLAYER_PED, -1), 0, 16, 0, 0);
+        }
+
+    }
+}
